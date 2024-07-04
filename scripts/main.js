93a6c5fb709182bfdabd2f16f035bd3256b6ee7e
Jab <164631211+Jab005@users.noreply.github.com>
2024-07-04 20:49:28 +0700

    Scripd
    Updat later

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,20 @@
-let modMindExp = Vars.mods.locateMod("mindustry-expansion").meta;
+const modMindExp = Vars.mods.locateMod("mindustry-expansion").meta;
 
 modMindExp.displayName = Core.bundle.get("mod.name");
 modMindExp.author = Core.bundle.get("mod.author");
 modMindExp.description = Core.bundle.get("mod.description");
 
-/* --- This code will be updated later by Jab when new Deltais has been finished! ---
+/*<===> [ LET THIS CODE ] <===>
+Will be updated later by Jab when Deltais is ready!
 
-modMindExp.softDependencies = ["rotatemindera"];
-let extraDependencies = Vars.mods.locateMod("rotatemindera");
+var extraDependencies = Vars.mods.locateMod("rotatemindera");
 
-if(extraDependencies != null){
-  modMindExp.description = Core.bundle.get("mod.description");
-  modMindExp.author = Core.bundle.get("mod.author-legacy"); -> Unavailable for now, don't change
+if(extraDependencies!=null){
+  Log.info(Core.bundle.get("log.active-dependencies"));
+  modMindExp.author = Core.bundle.get("mod.author") + Core.bundle.get("mod.contributor-dependencies");
+  modMindExp.description = Core.bundle.get("mod.description") + Core.bundle.get("mod.description-dependencies");
 }else{
-  Log.info("[accent]<MINDUSTRY-EXPANSION-LOG>\n[yellow]Missing mod soft-dependencies: 'rotatemindera'");
-  modMindExp.description = Core.bundle.get("mod.description")+Core.bundle.get("mod.dependencies-warning"); -> still unavailable, don't change
-}*/
+  Log.info(Core.bundle.get("log.missing-dependencies"));
+  modMindExp.author = Core.bundle.get("mod.author") + Core.bundle.get("mod.contributor-old");
+  modMindExp.description = Core.bundle.get("mod.description") + Core.bundle.get("mod.description-no-dependencies");
+};*/
