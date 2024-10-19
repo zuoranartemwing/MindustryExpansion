6ec72590c9a5333fc2e94e242a0dcf9c6b113f64
Neon <78184988+NEON-XZR@users.noreply.github.com>
2024-10-19 15:11:17 +0700

    updated script for music

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,16 @@
+//sound = require("sound");
+
 const modMindExp = Vars.mods.locateMod("mindustry-expansion").meta;
 
 modMindExp.displayName = Core.bundle.get("mod.name");
 modMindExp.author = Core.bundle.get("mod.author");
 modMindExp.description = Core.bundle.get("mod.description");
+
+// music
+const bossmusic1 = Vars.tree.loadMusic("arachnotheme");
+const ambimusic1 = Vars.tree.loadMusic("CTTW");
+
+Events.on(ClientLoadEvent, e => {
+    Vars.control.sound.bossMusic.add(bossmusic1);
+    Vars.control.sound.ambientMusic.add(ambimusic1);
+});
